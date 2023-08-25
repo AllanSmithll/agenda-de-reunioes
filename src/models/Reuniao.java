319a135518d6b0142b6090a27b8edc9bb314f4c0
AllanSmithll <101681192+AllanSmithll@users.noreply.github.com>
2023-08-25 14:35:45 -0300

    Update Reuniao.java

--- src/models/Reuniao.java
+++ src/models/Reuniao.java
@@ -11,58 +11,71 @@ public class Reuniao {
 	private int id;
 	private String data;
 	private List<Pessoa> listaDePessoas = new ArrayList<>();
-	
+
 	public Reuniao(String dataDaReuniao) {
 		data = dataDaReuniao;
 	}
 
-	public int getId() { return id;	}
-	public void setId(int id) {	this.id = id; }
-	public String getData() {return data; }
-	public void setData(String data) { this.data = data; }
-	public List<Pessoa> getListaDePessoas() { return listaDePessoas; }
-	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public List<Pessoa> getListaDePessoas() {
+		return listaDePessoas;
+	}
+
 	public void adicionarPessoa(Pessoa pessoa) {
 		this.listaDePessoas.add(pessoa);
 		pessoa.adicionarReuniao(this);
 	}
-	
+
 	public void removerPessoa(Pessoa pessoa) {
 		this.listaDePessoas.removeIf(p -> p.equals(pessoa));
 		pessoa.removerReuniao(this);
 	}
-	
+
 	public int numeroPessoasDaReuniao() {
 		return this.listaDePessoas.size();
 	}
 
- @Override 
- public boolean equals(Object obj) { 
-    if (this == obj) { 
-       return true; 
-    } 
-    if (obj == null || getClass() != obj.getClass()) { 
-       return false; 
-    } 
-    
-    Reuniao reuniao = (Reuniao) obj; 
-    return id.equals(reuniao.id); 
- }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null || getClass() != obj.getClass()) {
+			return false;
+		}
+
+		Reuniao reuniao = (Reuniao) obj;
+		return id == reuniao.id;
+	}
 
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-        sb.append("Reuniao: id=").append(id).append(", data=").append(data)
-        .append("\n"+" Pessoas:");
-        if(listaDePessoas.isEmpty()) {
-            sb.append("Nenhuma pessoa agendada");
-        } else {
-            sb.append("\n");
-            for (Pessoa p : listaDePessoas) {
-                sb.append(" * ").append(p.getNome()).append("\n");
-            }
-        }
-        
-        return sb.toString();	
-    }	
+		sb.append("Reuniao: id=").append(id).append(", data=").append(data).append("\n" + " Pessoas:");
+		if (listaDePessoas.isEmpty()) {
+			sb.append("Nenhuma pessoa agendada");
+		} else {
+			sb.append("\n");
+			for (Pessoa p : listaDePessoas) {
+				sb.append(" * ").append(p.getNome()).append("\n");
+			}
+		}
+
+		return sb.toString();
+	}
 }
