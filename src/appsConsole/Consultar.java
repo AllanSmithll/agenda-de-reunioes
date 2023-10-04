b6116c8f0c8b488843aa07dca96e4c10abfcafd3
Márcio J. Silva <105254896+ImMarcio@users.noreply.github.com>
2023-10-04 16:46:57 -0300

    Update Consultar.java
    Utilizando a fachada

--- src/appsConsole/Consultar.java
+++ src/appsConsole/Consultar.java
@@ -1,74 +1,55 @@
+/**********************************
+ * IFPB - Curso Superior de Tec. em Sist. para Internet
+ * POB - Persistencia de Objetos
+ * Prof. Fausto Ayres
+ *
+ */
+
 package appsConsole;
 
 import java.util.List;
 
-import com.db4o.ObjectContainer;
-import com.db4o.query.Candidate;
-import com.db4o.query.Evaluation;
-import com.db4o.query.Query;
-
 import models.Pessoa;
 import models.Reuniao;
+import regras_negocio.Fachada;
 
-public class Consultar {
-    protected ObjectContainer manager;
+public class ConsultaFachada {
 
-	public Consultar() {
+	public ConsultaFachada() {
 		try {
-			manager = Util.conectarBanco();
-			
+			Fachada.inicializar();
+
 			System.out.println("Consultas na Agenda de Reunioes... \n");
 			System.out.println("\n1- Quais as reunioes na data X?");
 			System.out.println("Data escolhida: 05/09/2023");
-			Query q;
-			q = manager.query();
-			q.constrain(Reuniao.class);
-			q.descend("data").constrain("05/09/2023");
-			List<Reuniao> reunioes = q.execute();
-			for(Reuniao r : reunioes)
+
+			for (Reuniao r : Fachada.listarReunioesNaData("05/09/2023")) {
 				System.out.println(r);
-			
-			
+			}
+
 			System.out.println("\n2- Quais as reunioes com a pessoa de nome X?");
 			System.out.println("Nome escolhido: Allan");
-			q = manager.query();
-			q.constrain(Reuniao.class);
-			q.descend("listaDePessoas").descend("nome").constrain("Allan");
-			List<Reuniao> reunioes_pessoa = q.execute();
-			for(Reuniao r : reunioes_pessoa)
+
+			for (Reuniao r : Fachada.listarReunioesComAPessoa("Allan")) {
 				System.out.println(r);
-			
+			}
+
 			System.out.println("\n3- Quais as pessoas que tem mais de N reunioes?");
 			System.out.println("Quantidade escolhida: 2");
-			q = manager.query();
-			q.constrain(Pessoa.class);
-			q.constrain(new Filtro1());
-			List<Pessoa> pessoas = q.execute();
-			for(Pessoa p :pessoas)
+
+			for (Pessoa p : Fachada.pessoasEmMaisDeNReunioes(2)) {
 				System.out.println(p);
-			
+			}
+
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 
-		Util.desconectar();
+		Fachada.finalizar();
 		System.out.println("\nfim do programa !");
 	}
 
 	public static void main(String[] args) {
 		new Consultar();
-	}
-}
-
-// class interna
-@SuppressWarnings("serial")
-class Filtro1 implements Evaluation {
-	public void evaluate(Candidate candidate) {
-		Pessoa p = (Pessoa) candidate.getObject();
-		int quantidade = 2;
-		if(p.getReunioes().size() > quantidade) 
-			candidate.include(true);
-		else		
-			candidate.include(false);
 	}
 }
