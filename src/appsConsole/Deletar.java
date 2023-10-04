f0ebc897e692be5b9e23a3988587bba7969b47b0
Márcio J. Silva <105254896+ImMarcio@users.noreply.github.com>
2023-10-04 17:00:16 -0300

    Update Deletar.java
    utilizando a fachada

--- src/appsConsole/Deletar.java
+++ src/appsConsole/Deletar.java
@@ -12,6 +12,7 @@ import com.db4o.query.Query;
 
 import models.Pessoa;
 import models.Reuniao;
+import regras_negocio.Fachada;
 
 
 public class Deletar {
@@ -19,49 +20,19 @@ public class Deletar {
 
 	public Deletar() {
 		try {
-			manager = Util.conectarBanco();
-			System.out.println("excluindo");
+			Fachada.inicializar();
 			
-			Query queryReuniao = manager.query();
-			queryReuniao.constrain(Reuniao.class);  							 
-			List<Reuniao> reunioes = queryReuniao.execute(); 
+			//Excluindo a pessoa Márcio
+			Fachada.excluirPessoa("Marcio");
+			System.out.println("Pessoa excluida");
 			
-			Query queryPessoa = manager.query();
-			queryPessoa.constrain(Pessoa.class); 
-			queryPessoa.descend("nome").constrain("Marcio");
-			List<Pessoa> pessoas = queryPessoa.execute(); // select p from Pessoa p where p.nome="Marcio"
-			
-			Pessoa marcio = pessoas.get(0);
-	
-			//Removendo todas as reunioes agendadas da pessoa Marcio,
-			//e caso a reuniao não tenha mais membros ela sera deletada
-			if(pessoas.size() > 0) {
-				for (Reuniao reuniao : reunioes) {
-					if(reuniao.getParticipanteReuniao(marcio)) {
-						reuniao.removerPessoa(marcio);
-						manager.store(reuniao);	
-						marcio.removerReuniao(reuniao);
-						manager.store(marcio);;
-					} 
 					
-				}
-			}
-			
-			//Deletando todas as reuniões orfãos
-			for (Reuniao reuniao : reunioes) {
-				if(reuniao.numeroPessoasDaReuniao() < 2) {
-					manager.delete(reuniao);
-					
-				}
-			}
-
-			manager.commit();
 			}
 		 catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 
-		Util.desconectar();
+		Fachada.finalizar();
 		System.out.println("\nfim do programa !");
 	}
 
