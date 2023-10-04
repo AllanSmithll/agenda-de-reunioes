e6e773ddcfd93da58add43b585309e67e7755538
AllanSmithll <101681192+AllanSmithll@users.noreply.github.com>
2023-10-04 17:43:42 -0300

    Update Alterar.java

--- src/appsConsole/Alterar.java
+++ src/appsConsole/Alterar.java
@@ -1,16 +1,15 @@
 package appsConsole;
 
-import appsConsole.AlterarFachada;
 import regras_negocio.Fachada;
 
 public class Alterar {
-	
+
 	public Alterar() {
 		try {
 			Fachada.inicializar();
 			Fachada.removerPessoaDaReuniao("Marcio", 1);
 			System.out.println("Pessoa Márcio removido da reuniao com id 1 ");
-			
+
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
